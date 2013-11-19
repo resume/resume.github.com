8c5b3fa59514bfcd0678a288d0cee23e2541af3d
David Coallier <dcoallier@engineyard.com>
2013-11-19 14:22:44 +0000

    Opted more users out. Thats all I could catch in my twitter stream.

--- js/opt_out_users.js
+++ js/opt_out_users.js
@@ -2,5 +2,7 @@ var githubresume_opt_out_users = [
     "augustl",
     "kmcallister",
     "ntlk",
-    "raganwald"
+    "raganwald",
+    "mattfield", //"https://twitter.com/mattfieldy/status/402432266208821248"
+    "evilchili"
 ];
